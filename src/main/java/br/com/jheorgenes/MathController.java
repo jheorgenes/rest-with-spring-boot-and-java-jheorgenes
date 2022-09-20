da5c1fdb28daa19dfea9a873a4bd925b62c666f6
Jheorgenes Warlley <jheorgenes@gmail.com>
2022-09-20 07:27:53 -0300

    Ajustes rápidos no projeto

---
+++ src/main/java/br/com/jheorgenes/MathController.java
@@ -0,0 +1,43 @@
+package br.com.jheorgenes;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.jheorgenes.exceptions.UnsupportedMathOperationException;
+
+
+@RestController
+public class MathController {
+
+	private final AtomicLong counter = new AtomicLong();
+
+	@RequestMapping(value = "/sum/{numberOne}/{numberTwo}", method = RequestMethod.GET)
+	public Double sum(
+			@PathVariable(value = "numberOne") String numberOne,
+			@PathVariable(value = "numberTwo") String numberTwo) throws Exception {
+		
+		if(!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+			throw new UnsupportedMathOperationException("Please set a numeric value!");
+		}
+		return convertToDouble(numberOne) + convertToDouble(numberTwo);
+	}
+
+	private Double convertToDouble(String strNumber) {
+		if(strNumber == null) return 0D;
+		
+		String number = strNumber.replaceAll(",", "."); //Substitui a vírgula por ponto
+		if(isNumeric(number)) return Double.parseDouble(number);
+		
+		return 0D;
+	}
+
+	private boolean isNumeric(String strNumber) {
+		if(strNumber == null) return false;
+		String number = strNumber.replaceAll(",", ".");
+		return number.matches("[-+]?[0-9]*\\.?[0-9]+"); //Valida se é um valor numérico
+	}
+}
---
+++ src/main/java/br/com/jheorgenes/exceptions/handler/CustomizeResponseEntityExceptionHandler.java
@@ -0,0 +1,38 @@
+package br.com.jheorgenes.exceptions.handler;
+
+import java.util.Date;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import br.com.jheorgenes.exceptions.ExceptionResponse;
+
+@ControllerAdvice
+@RestController
+public class CustomizeResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+	
+	@ExceptionHandler(Exception.class)
+	public final ResponseEntity<ExceptionResponse> handleAllExceptions(Exception ex, WebRequest request){
+		ExceptionResponse exceptionResponse = new ExceptionResponse(
+			new Date(), 
+			ex.getMessage(), 
+			request.getDescription(false)
+		);
+		return new ResponseEntity<>(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+	
+	@ExceptionHandler(UnsupportedOperationException.class)
+	public final ResponseEntity<ExceptionResponse> handleDadRequestExceptions(Exception ex, WebRequest request){
+		ExceptionResponse exceptionResponse = new ExceptionResponse(
+			new Date(), 
+			ex.getMessage(), 
+			request.getDescription(false)
+		);
+		return new ResponseEntity<>(exceptionResponse, HttpStatus.BAD_REQUEST);
+	}
+}
